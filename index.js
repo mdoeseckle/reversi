61907cb687e4ee4e87e5a4f85f921ce02995b5fe
Mark Doeseckle <mark.doeseckle@chicagotrading.com>
2012-07-06 17:44:45 -0500

    add comments

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ var pair = function(pattern, handle) {
 
 var handle = []
 handle.push(pair(/^$/, requestHandlers.game))
-//handle.push(pair(/^[\w|\d]+$/, requestHandlers.game))
 handle.push(pair(/^static\/\w+/, requestHandlers.static))
+
+// when supporting multiple simultaneous games, use these and remove the root handler above:
+//handle.push(pair(/^$/, requestHandlers.newGame))
+//handle.push(pair(/^[\w|\d]+$/, requestHandlers.game))
 
 server.start(router.route, handle)
